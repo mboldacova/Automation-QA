bef9bbf5306786d12c58fa90c68a80fc312d059f
mboldacova <barcaforall@gmail.com>
2024-01-21 14:31:14 -0800

    New files

---
+++ lesson_1_task_4.py
@@ -0,0 +1,10 @@
+def first_name():
+    print("Your name is " + first, end=" ")
+
+def last_name():
+    print(last)
+
+first=input("What is your first name? ")
+last=input("What is your last name? ")
+first_name()
+last_name()
---
+++ lesson_1_task_7.py
@@ -0,0 +1,51 @@
+def P_Var1():
+    Var1=1
+    print(Var1, end="")
+
+def P_Var2():
+    Var2=2
+    print(Var2, end="")
+
+def P_Var3():
+    Var3=3
+    print(Var3, end="")
+
+def P_Var4():
+    Var4=4
+    print(Var4, end="")
+
+def P_Var5(): 
+    Var5=5
+    print(Var5, end="")
+
+def P_Var6():
+    Var6=6
+    print(Var6, end="")
+
+def P_Var7():
+    Var7=7
+    print(Var7, end="")
+
+def P_Var8():
+    Var8=8
+    print(Var8, end="")
+    
+def P_Var9():
+    Var9=9
+    print(Var9, end="")
+
+def P_Var0():
+    Var0=0
+    print(Var0, end="")
+
+P_Var8()
+P_Var8()
+P_Var0()
+P_Var0()
+P_Var5()
+P_Var5()
+P_Var5()
+P_Var3()
+P_Var5()
+P_Var3()
+P_Var5()
